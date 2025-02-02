56658eade56581b25863e67182e10db016341242
Ani822 <anihovakimyan001@gmail.com>
2025-02-03 00:33:07 +0400

    update images file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Footer from './footer/Footer';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
