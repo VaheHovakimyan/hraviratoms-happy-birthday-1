418120cdc3f59b9046dcfac1385a774231b24529
Vahe4836 <88284245+Vahe4836@users.noreply.github.com>
2025-03-13 00:12:28 +0400

    change video player

--- src/components/main-bg/Main.tsx
+++ src/components/main-bg/Main.tsx
@@ -1,13 +1,12 @@
 import { FC } from 'react';
 import ContactOwnerForm from '../contact-owner/ContactOwnerForm';
 import './Main.scss';
-import ReactPlayer from 'react-player';
 import video from '../../images/fixed/video.mp4';
 
 const Main: FC = () => {
     return (
         <div className="main_fixed_image">
-            <ReactPlayer url={video} playing={true} loop width={'100%'} height={'100%'} />
+            <video src={video} autoPlay loop muted/>
             <ContactOwnerForm />
         </div>
     )
