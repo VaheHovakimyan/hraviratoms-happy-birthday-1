b1146197b72c6e4e8dc8f23d51778ec5d736b968
Vahe4836 <88284245+Vahe4836@users.noreply.github.com>
2025-03-17 23:47:46 +0400

    add animations

--- src/components/main-bg/Main.tsx
+++ src/components/main-bg/Main.tsx
@@ -12,15 +12,26 @@ const Main: FC = () => {
 
     return (
         <div className="main_fixed_image">
+            <div className='main_opacity'>
             {/* <video src={video} autoPlay loop muted/> */}
-            <ContactOwnerForm />
-            <div data-aos="fade-up">
-                <h1>Hello</h1>
+            <div data-aos="fade-right"
+                data-aos-offset="300"
+                data-aos-easing="ease-in-sine">
+                <h1 className='invite_title'>Սիրով հրավիրում ենք Ձեզ նշելու Դավիթ Հարությունյանի 20 ամյակը</h1>
+            </div>
+            {/* <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+                <div data-aos="zoom-in">
+                    <div style={{ width: '500px', height: '400px', background: 'blue' }}></div>
+                </div>
             </div>
             <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
                 <div data-aos="zoom-in">
-                    <div style={{ width: '500px', height: '400px', background: 'blue'}}></div>
+                    <div style={{ width: '500px', height: '400px', background: 'blue' }}></div>
                 </div>
+            </div> */}
+            <div data-aos="zoom-in">
+                <ContactOwnerForm />
+            </div>
             </div>
         </div>
     )
