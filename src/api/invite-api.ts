066c13e1bf2a38bc9f4a72d505cfe547bb19ed37
Vahe4836 <88284245+Vahe4836@users.noreply.github.com>
2025-04-09 23:21:21 +0400

    add toast

--- src/api/invite-api.ts
+++ src/api/invite-api.ts
@@ -1,7 +1,8 @@
+import toast from "react-hot-toast";
 import { IGuest } from "../common/type";
 
 export const sendInviteRequest = async (values: IGuest) => {
-  await fetch(`${process.env.REACT_APP_API_PROD}/api/hraviratoms-happy-day/create`, {
+  return await fetch(`${process.env.REACT_APP_API_PROD}/api/hraviratoms-happy-day/create`, {
     method: "POST",
     body: JSON.stringify(values),
     headers: {
--- src/components/contact-owner/ContactOwnerForm.tsx
+++ src/components/contact-owner/ContactOwnerForm.tsx
@@ -8,6 +8,7 @@ import { IGuest } from "../../common/type";
 import { Button } from "@mui/material";
 import { userSchema } from "../../common/schema";
 import './ContactOwnerForm.scss';
+import toast, { Toaster } from "react-hot-toast";
 
 const initialValue: IGuest = {
   name: '',
@@ -20,101 +21,121 @@ const ContactOwnerForm: FC = () => {
   const [phone, setPhone] = useState<string>();
 
   return (
-    <Formik
-      validateOnMount
-      validateOnChange
-      initialValues={initialValue}
-      validationSchema={userSchema}
-      onSubmit={async (values) => {
-        await sendInviteRequest({ ...values, phone });
-      }}
-    >
-      {({ values, isValid, handleChange }) => (
-        <Form>
-          <div className="contact_main_div" id="form">
-            <h3 className="contact_div_title">Պատասխան կարող եք ուղարկել մինչև 15.07.2025թ</h3>
+    <>
+      <Formik
+        validateOnMount
+        validateOnChange
+        initialValues={initialValue}
+        validationSchema={userSchema}
+        onSubmit={async (values) => {
+          const response = await sendInviteRequest({ ...values, phone });
+          console.log('response', response);
 
-            <div className="contact_form">
-              <div className="contact_form_input_div">
-                <div className="contact_form_input_div_span">
-                  <p className="contact_form_input_span">Անուն Ազգանուն</p>
-                  <div className="contact_form_input_div_border">
-                    <div className="contact_form_input_and_icon_div">
-                      <BadgeIcon className="contact_form_input_icon" sx={{ fontSize: 37 }} />
-                      <Field
-                        id="name"
-                        name="name"
-                        type="text"
-                        placeholder="Անուն Ազգանուն"
-                        onChange={handleChange}
-                        className="contact_input"
-                      />
+          if (response.ok) {
+            console.log("Success");
+
+            toast.success("Հարգելի հյուր Ձեր մասնակցության հայտը մեծ սիրով ընդունվեց❤️");
+          } else {
+            toast.error("Տեղի ունեցավ խնդիր, փորձեք մի փոքր ուշ😔");
+          }
+        }}
+      >
+        {({ values, isValid, handleChange }) => (
+          <Form>
+            <Toaster 
+             position="top-right"
+             toastOptions={{
+               duration: 2000,
+               style: {
+                 marginTop: '120px'
+               },
+             }}
+            />
+            <div className="contact_main_div" id="form">
+              <h3 className="contact_div_title">Պատասխան կարող եք ուղարկել մինչև 15.07.2025թ</h3>
+
+              <div className="contact_form">
+                <div className="contact_form_input_div">
+                  <div className="contact_form_input_div_span">
+                    <p className="contact_form_input_span">Անուն Ազգանուն</p>
+                    <div className="contact_form_input_div_border">
+                      <div className="contact_form_input_and_icon_div">
+                        <BadgeIcon className="contact_form_input_icon" sx={{ fontSize: 37 }} />
+                        <Field
+                          id="name"
+                          name="name"
+                          type="text"
+                          placeholder="Անուն Ազգանուն"
+                          onChange={handleChange}
+                          className="contact_input"
+                        />
+                      </div>
                     </div>
                   </div>
-                </div>
 
-                <div className="contact_form_input_div_span">
-                  <p className="contact_form_input_span">Հեռախոսահամար</p>
-                  <div className="contact_form_input_div_border">
-                    <div className="contact_form_input_and_icon_div" style={{ padding: '12px 16px' }}>
-                      <PhoneInput
-                        defaultCountry="am"
-                        value={phone}
-                        onChange={(phone: string) => setPhone(phone)}
-                        style={{
-                          width: '100%',
-                          height: '100%',
-                          border: 'none',
-                          outline: 'none',
-                        }}
-                      />
+                  <div className="contact_form_input_div_span">
+                    <p className="contact_form_input_span">Հեռախոսահամար</p>
+                    <div className="contact_form_input_div_border">
+                      <div className="contact_form_input_and_icon_div" style={{ padding: '12px 16px' }}>
+                        <PhoneInput
+                          defaultCountry="am"
+                          value={phone}
+                          onChange={(phone: string) => setPhone(phone)}
+                          style={{
+                            width: '100%',
+                            height: '100%',
+                            border: 'none',
+                            outline: 'none',
+                          }}
+                        />
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="contact_form_input_div_span">
-                  <p className="contact_form_input_span">Հյուրերի քանակ</p>
-                  <div className="contact_form_input_div_border">
-                    <div className="contact_form_input_and_icon_div">
-                      <GroupIcon className="contact_form_input_icon" sx={{ fontSize: 37 }} />
-                      <Field
-                        id="guestCount"
-                        name="guestCount"
-                        type="number"
-                        placeholder="Հյուրերի քանակ"
-                        onChange={handleChange}
-                        className="contact_input"
-                      />
+                  <div className="contact_form_input_div_span">
+                    <p className="contact_form_input_span">Հյուրերի քանակ</p>
+                    <div className="contact_form_input_div_border">
+                      <div className="contact_form_input_and_icon_div">
+                        <GroupIcon className="contact_form_input_icon" sx={{ fontSize: 37 }} />
+                        <Field
+                          id="guestCount"
+                          name="guestCount"
+                          type="number"
+                          placeholder="Հյուրերի քանակ"
+                          onChange={handleChange}
+                          className="contact_input"
+                        />
+                      </div>
                     </div>
                   </div>
-                </div>
 
+                </div>
+                <div className="contact_div_general_2">
+                  <p className="contact_paragraph">Արարողությանը ներկա ե՞ք լինելու</p>
+
+                  <div id="contact_input">
+                    <div className="contact_input_flex">
+                      <Field type="radio" name="isVisiting" id="Yes" value="Yes" />
+                      <label className="contact_radio_label">Այո</label>
+                    </div>
+                    <div className="contact_input_flex">
+                      <Field type="radio" name="isVisiting" id="No" value="No" />
+                      <label className="contact_radio_label">Ոչ</label>
+                    </div>
+                  </div>
+
+                  <Button
+                    className={isValid ? "contact_button" : "contact_button_disabled"}
+                    type="submit"
+                    disabled={!isValid}
+                  >Ուղարկել</Button>
+                </div>
               </div>
-              <div className="contact_div_general_2">
-                <p className="contact_paragraph">Արարողությանը ներկա ե՞ք լինելու</p>
 
-                <div id="contact_input">
-                  <div className="contact_input_flex">
-                    <Field type="radio" name="isVisiting" id="Yes" value="Yes" />
-                    <label className="contact_radio_label">Այո</label>
-                  </div>
-                  <div className="contact_input_flex">
-                    <Field type="radio" name="isVisiting" id="No" value="No" />
-                    <label className="contact_radio_label">Ոչ</label>
-                  </div>
-                </div>
-
-                <Button
-                  className={isValid ? "contact_button" : "contact_button_disabled"}
-                  type="submit"
-                  disabled={!isValid}
-                >Ուղարկել</Button>
-              </div>
             </div>
-
-          </div>
-        </Form>
-      )}
-    </Formik >
+          </Form>
+        )}
+      </Formik>
+    </>
   )
 }
 
